bbb23142ebfbac5d3c9b2621f3773189951d788e
nigmasilmi <nigmasilmi.web@gmail.com>
2020-02-18 13:59:52 -0300

    added detail news view

--- artdet/urls.py
+++ artdet/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import NoticiaDetail
 
 urlpatterns = [
-    # path('/<int:pk>/', NoticiaDetail, name='detalle-noticia'),
-    path('', NoticiaDetail, name='detalle-noticia'),
+    path('<int:noticia_id>/', NoticiaDetail, name='noticia'),
+    # path('', NoticiaDetail, name='noticia'),
 ]
--- artdet/views.py
+++ artdet/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from articles.models import News
 from articles.modelsMore import Categoria
 
 
-def NoticiaDetail(request, pk):
+def NoticiaDetail(request, noticia_id):
     # la url debe venir con el pk y luego hacemos fetch con ese pk
     template_path = 'artdet/article_detail.html'
-    noticias_qs = News.objects.all()
-    noticia = News.object.filter(id=pk)
-    context = {'noticias': noticias_qs, 'noticia': noticia}
+    # noticias_qs = News.objects.all()
+    # noticia = News.objects.filter(id=noticia_id)
+    noticia = get_object_or_404(News, pk=noticia_id)
+    context = {'noticia': noticia}
     return render(request, template_path, context)
